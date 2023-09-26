439a190184318fcd2e08b3a4b46443507efccd7a
karthi <ekece94@gmail.com>
2023-09-26 13:37:15 +0530

    destroy all

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
+/*
 output "jenkins" {
    value = aws_instance.jenkins.public_ip
 #  value = [ for instance in aws_instance.server: instance.public_ip ]
 }
-/*
 output "tomcat" {
     value = aws_instance.server["tomcat"].public_ip
 }
